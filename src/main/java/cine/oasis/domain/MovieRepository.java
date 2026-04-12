7d0ca8d87e430612c8ed8940656268a8e697d02a
joselopez02-cyber <tu_correo@ejemplo.com>
2026-04-12 13:51:52 -0500

    Limpieza final de archivos bloqueados

--- src/main/java/cine/oasis/domain/MovieRepository.java
+++
@@ -1,7 +0,0 @@
-package cine.oasis.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MovieRepository extends JpaRepository<Movie, Long> { }
